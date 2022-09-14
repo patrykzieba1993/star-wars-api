6d379d6804770f661795e2aa1d649ac95692ab51
Patryk Zieba <patryk.zieba@schibsted.pl>
2022-09-14 18:54:47 +0200

    chore: init the project

---
+++ handler.js
@@ -0,0 +1,11 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello Star Wars!",
+  });
+});
+
+module.exports.handler = serverless(app);
