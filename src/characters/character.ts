36b5a6ed8e8dfcb426f2c364f1737815a89ddd0f
Patryk Zieba <patryk.zieba@schibsted.pl>
2022-09-14 21:56:11 +0200

    feat: create naiave implementation of characters in memory repository

--- src/characters/character.ts
+++ src/characters/character.ts
@@ -5,4 +5,12 @@ type Character = {
   planet?: string,
 };
 
-export { Character };
+interface CharactersRepository {
+  getAll: (offset?: number, limit?: number) => Promise<Array<Character>>;
+  getById: (id: string) => Promise<Character>,
+  create: (character: Character) => Promise<Character>,
+  update: (character: Partial<Character>) => Promise<Character>,
+  remove: (id: string) => Promise<Character>,
+}
+
+export { Character, CharactersRepository };
---
+++ src/characters/characters.repository/inMemory.ts
@@ -0,0 +1,34 @@
+import { Character, CharactersRepository } from "../character";
+
+// collection parameter is mutable intentionally because it simulates a permanent data storage
+const createCharactersInMemoryRepository = (collection: Array<Character> = []): CharactersRepository => {
+  const getAll = (offset?: number, limit?: number) => {
+    return Promise.resolve(collection);
+  };
+
+  const getById = (id: string) => {
+    return Promise.resolve(collection[0]);
+  };
+
+  const create = (character: Character) => {
+    return Promise.resolve(collection[0]);
+  };
+
+  const update = (character: Partial<Character>) => {
+    return Promise.resolve(collection[0]);
+  };
+
+  const remove = (id: string) => {
+    return Promise.resolve(collection[0]);
+  };
+
+  return {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+  }
+};
+
+export { createCharactersInMemoryRepository };
