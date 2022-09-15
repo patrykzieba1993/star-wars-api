ff255df7401e955b1b7c820e47e159945719f080
Patryk Zieba <patryk.zieba@schibsted.pl>
2022-09-15 17:56:39 +0200

    feat: implement characters service basing on unit tests

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,32 +1,60 @@
-import { Character, CharacterDTO } from "./character";
-import { CharactersRepository } from "./character";
+import { Character, CharacterDTO } from './character';
+import { CharactersRepository } from './character';
 import { generateUUID } from '../uuid/generate';
+import { ResourceNotFoundError } from '../errors';
 
 interface CharactersService {
-  getById: (id: string) => Promise<Character | null>,
-  create: (characterDTO: CharacterDTO) => Promise<Character | null>,
-  update: (id: string, characterDTO: CharacterDTO) => Promise<Character | null>,
-  remove: (id: string) => Promise<Character | null>,
+  getById: (id: string) => Promise<Character>,
+  create: (characterDTO: CharacterDTO) => Promise<Character>,
+  update: (id: string, characterDTO: CharacterDTO) => Promise<Character>,
+  remove: (id: string) => Promise<Character>,
 }
+
+const CharacterNotFoundError = (id: string) => {
+  return new ResourceNotFoundError(`Character with id ${id} not found`);
+};
 
 const createCharactersService = (
   charactersRepository: CharactersRepository,
   generateUniqueId = generateUUID,
 ): CharactersService => {
   const getById = async (id: string) => {
-    return null;
+    const characterFound = await charactersRepository.getById(id);
+
+    if (!characterFound) {
+      throw CharacterNotFoundError(id);
+    }
+
+    return characterFound;
   };
 
   const create = async (characterDTO: CharacterDTO) => {
-    return null;
+    const character: Character = {
+      id: generateUniqueId(),
+      ...characterDTO,
+    };
+
+    return charactersRepository.create(character);
   };
 
   const update = async (id: string, characterDTO: CharacterDTO) => {
-    return null;
+    const characterUpdated = await charactersRepository.update(id, characterDTO);
+
+    if (!characterUpdated) {
+      throw CharacterNotFoundError(id);
+    }
+
+    return characterUpdated;
   };
 
   const remove = async (id: string) => {
-    return null;
+    const characterRemoved = await charactersRepository.remove(id);
+
+    if (!characterRemoved) {
+      throw CharacterNotFoundError(id);
+    }
+
+    return characterRemoved;
   };
 
   return {
