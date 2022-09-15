9cb51d490e827901aeaad3c7a664571bcc5f2f3a
Patryk Zieba <patryk.zieba@schibsted.pl>
2022-09-15 16:36:48 +0200

    feat: implement characters mongo repository basing on unit tests

--- src/characters/characters.repository/mongo.ts
+++ src/characters/characters.repository/mongo.ts
@@ -1,31 +1,50 @@
 import { Db } from 'mongodb';
 
-import { Character, CharactersRepository } from "../character";
+import { Character, CharactersRepository } from '../character';
+
+const findOptions = { projection: { _id: 0 } };
 
 const defaults = {
   offset: 0,
   limit: 100,
+  collectionName: 'characters',
 };
 
 const createCharactersMongoRepository = (database: Db): CharactersRepository => {
-  const getAll = (offset?: number, limit?: number) => {
-    return Promise.resolve([]);
+  const collection = database.collection<Character>(defaults.collectionName);
+
+  const getAll = async (offset?: number, limit?: number) => {
+    const charactersFound = await collection
+      .find({}, findOptions)
+      .skip(offset || defaults.offset)
+      .limit(limit || defaults.limit)
+      .toArray();
+
+    return charactersFound;
   };
 
-  const getById = (id: string) => {
-    return Promise.resolve(null);
+  const getById = async (id: string) => {
+    return collection.findOne({ id }, findOptions);
   };
 
-  const create = (character: Character) => {
-    return Promise.resolve(character);
+  const create = async (character: Character) => {
+    await collection.insertOne({ ...character });
+
+    return character;
   };
 
-  const update = (id: string, character: Partial<Character>) => {
-    return Promise.resolve(null);
+  const update = async (id: string, character: Partial<Character>) => {
+    await collection.updateOne({ id }, { $set: { ...character } });
+
+    return getById(id);
   };
 
-  const remove = (id: string) => {
-    return Promise.resolve(null);
+  const remove = async (id: string) => {
+    const characterRemoved = await getById(id);
+
+    await collection.deleteOne({ id });
+
+    return characterRemoved;
   };
 
   return {
